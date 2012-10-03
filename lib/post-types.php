2f8df2cdf16a70a0c9205dfa5746dfef86113d8b
doofenshmirtz <blackcaps16@gmail.com>
2012-10-03 20:50:15 +0530

    added custom post slider

--- lib/post-types.php
+++ lib/post-types.php
@@ -1,3 +1,22 @@
 <?php
 
 // Custom post types
+
+/**
+ * Slider post type
+ */
+
+function slider_register() {
+
+  $args = array(
+    'label' => __('Slider'),
+    'public' => true,
+    'show_ui'=> true,
+    'rewrite' => true,    
+    'supports' => array('title', 'editor', 'thumbnail'),
+    'capability_type' => 'post',
+    );
+  register_post_type('image-slider' , $args);
+}
+
+add_action('init', 'slider_register');  
