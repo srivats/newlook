68160dc5f135ccd95dddc43617009472294791b7
doofenshmirtz <blackcaps16@gmail.com>
2012-10-18 08:13:56 +0530

    added footer links

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,21 @@
 <footer id="content-info" class="container" role="contentinfo">
   <?php dynamic_sidebar('sidebar-footer'); ?>
   <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+  <ul class="pull-left">
+  	<li><a href="#">Website Design</a></li>
+  	<li><a href="#">eCommerce Design</a></li>
+  	<li><a href="#">Magento Design</a></li>
+  	<li><a href="#">Brand Identity</a></li>  	
+  </ul>
+  <ul class="pull-left">
+	<li><a href="#">About us</a></li>
+	<li><a href="#">Portfolio</a></li>
+	<li><a href="#">Testimonials</a></li>
+	<li><a href="#">Blog</a></li>
+	<li><a href="#">Contact Us</a></li>
+  </ul>	
+
+
 </footer>
 
 <?php if (GOOGLE_ANALYTICS_ID) : ?>
