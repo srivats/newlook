7a1bfcc73487980d4ec38d55df7839eb16523e2b
doofenshmirtz <blackcaps16@gmail.com>
2012-10-18 22:31:40 +0530

    removed ul tag

--- lib/shortcodes/recent-posts.php
+++ lib/shortcodes/recent-posts.php
@@ -1,12 +1,10 @@
-<ul class="related-posts">
-	<li>
-		<div class="meta pull-left">
-			<span class="date"><?php the_time('d') ?></span><br>
-			<span class="month"><?php the_time('M')?></span>
-		</div>
-		<div>
-			<a href="<?php get_permalink()?>"><?php echo get_the_title() ?></a>
-			<p><?php echo the_excerpt()?></p>
-		</div>
-	</li>
-</ul>
+<li>
+	<div class="meta pull-left">
+		<span class="date"><?php the_time('d') ?></span><br>
+		<span class="month"><?php the_time('M')?></span>
+	</div>
+	<div>
+		<a href="<?php get_permalink()?>"><?php echo get_the_title() ?></a>
+		<p><?php echo the_excerpt()?></p>
+	</div>
+</li>
