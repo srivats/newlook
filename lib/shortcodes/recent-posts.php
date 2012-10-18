d4bb0b724519ba30ea350fdf89fbf50134b3fabb
doofenshmirtz <blackcaps16@gmail.com>
2012-10-18 22:13:17 +0530

    Add basic date & basic styles

--- lib/shortcodes/recent-posts.php
+++ lib/shortcodes/recent-posts.php
@@ -1,6 +1,12 @@
 <ul class="related-posts">
 	<li>
-		<a href="<?php get_permalink()?>"><?php echo get_the_title() ?></a>
-		<p><?php echo the_excerpt()?></p>
+		<div class="meta pull-left">
+			<span class="date"><?php the_time('d') ?></span><br>
+			<span class="month"><?php the_time('M')?></span>
+		</div>
+		<div>
+			<a href="<?php get_permalink()?>"><?php echo get_the_title() ?></a>
+			<p><?php echo the_excerpt()?></p>
+		</div>
 	</li>
 </ul>
