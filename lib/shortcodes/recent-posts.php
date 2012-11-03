656ca5ab572aea73843adda9596b71db06dc7e38
doofenshmirtz <blackcaps16@gmail.com>
2012-11-03 06:07:52 +0530

    added strong tag

--- lib/shortcodes/recent-posts.php
+++ lib/shortcodes/recent-posts.php
@@ -1,9 +1,9 @@
 <li>
-	<div class="meta pull-left">
-		<span class="date"><?php the_time('d') ?></span><br>
-		<span class="month"><?php the_time('M')?></span>
+	<div class="meta pull-left meta-rounder-corner span1">
+		<div class="date"><strong><?php the_time('d') ?></strong></div>
+		<div class="month"><?php the_time('M')?></div>
 	</div>
-	<div>
+	<div class="content">
 		<a href="<?php get_permalink()?>"><?php echo get_the_title() ?></a>
 		<p><?php echo the_excerpt()?></p>
 	</div>
