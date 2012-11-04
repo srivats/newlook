b809284a90c5df7a1cd987caa435bd4d666c3324
doofenshmirtz <blackcaps16@gmail.com>
2012-11-04 07:54:26 +0530

    fixed permalink

--- lib/shortcodes/recent-posts.php
+++ lib/shortcodes/recent-posts.php
@@ -4,7 +4,7 @@
 		<div class="month"><?php the_time('M')?></div>
 	</div>
 	<div class="content">
-		<h4><strong><a href="<?php get_permalink()?>"><?php echo get_the_title() ?></a></strong></h4>
+		<h4><strong><a href="<?php echo get_permalink()?>"><?php echo get_the_title() ?></a></strong></h4>
 		<p><?php echo the_excerpt()?></p>
 	</div>
 </li>
